f3866a25ba44c09a1abd45a5ae6da657ad0bf57f
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2024-09-10 02:33:31 +0300

    fix path

--- midi/Launchkey_Mini_MK4/elements.py
+++ midi/Launchkey_Mini_MK4/elements.py
@@ -1,7 +1,7 @@
 # decompyle3 version 3.9.2
 # Python bytecode version base 3.7.0 (3394)
 # Decompiled from: Python 3.12.5 (main, Aug  6 2024, 19:08:49) [Clang 15.0.0 (clang-1500.3.9.4)]
-# Embedded file name: output/Live/mac_universal_64_static/Release/python-bundle/MIDI Remote Scripts/Launchkey_Mini_MK4/elements.py
+# Embedded file name: /MIDI Remote Scripts/Launchkey_Mini_MK4/elements.py
 # Compiled at: 2024-08-28 00:56:43
 # Size of source mod 2**32: 609 bytes
 from __future__ import absolute_import, print_function, unicode_literals
